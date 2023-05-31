ec2ba60b6ebc9cbd10f70342160ba282d93f7fd9
victor850 <2559022015@gmail.com>
2023-05-31 17:07:38 -0600

    31052023

--- app/src/main/java/sv/edu/utec/parcial4_rodriguez/MainActivity.java
+++ app/src/main/java/sv/edu/utec/parcial4_rodriguez/MainActivity.java
@@ -1,14 +1,68 @@
 package sv.edu.utec.parcial4_rodriguez;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout=findViewById(R.id.drawer_Layout);
+        navigationView=findViewById(R.id.navigation_view);
+        actionBarDrawerToggle= new ActionBarDrawerToggle( this,drawerLayout, R.string.menu_Open, R.string.close_menu);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_asignatura:
+                        Toast.makeText(getApplicationContext(), "Asignatura" ,Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_evento:
+                        Toast.makeText(getApplicationContext(), "Evento" ,Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_horario:
+                        Toast.makeText(getApplicationContext(), "Horario" ,Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+
     }
 }
